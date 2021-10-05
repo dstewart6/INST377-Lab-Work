4881053eacffe6638668b68bc2ea5846475e8707
Monsterlaser <73480524+Monsterlaser@users.noreply.github.com>
2021-10-04 23:31:48 -0400

    Update script.js

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -50,4 +50,54 @@ document.addEventListener('DOMContentLoaded', () => {
       [1,width+1,width*2+1,width*3+1],
       [width,width+1,width+2,width+3]
     ]
+    
+    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+    let currentPosition = 4
+    let currentRotation = 0
+  
+    console.log(theTetrominoes[0][0])
+  
+    //randomly select a Tetromino and its first rotation
+    let random = Math.floor(Math.random()*theTetrominoes.length)
+    let current = theTetrominoes[random][currentRotation]
+  
+    //draw the Tetromino
+    function draw() {
+      current.forEach(index => {
+        squares[currentPosition + index].classList.add('tetromino')
+        squares[currentPosition + index].style.backgroundColor = colors[random]
+      })
+    }
+  
+    //undraw the Tetromino
+    function undraw() {
+      current.forEach(index => {
+        squares[currentPosition + index].classList.remove('tetromino')
+        squares[currentPosition + index].style.backgroundColor = ''
+  
+      })
+    }
+  
+    //assign functions to keyCodes
+    function control(e) {
+      if(e.keyCode === 37) {
+        moveLeft()
+      } else if (e.keyCode === 38) {
+        rotate()
+      } else if (e.keyCode === 39) {
+        moveRight()
+      } else if (e.keyCode === 40) {
+        moveDown()
+      }
+    }
+    document.addEventListener('keyup', control)
+  
+    //move down function
+    function moveDown() {
+      undraw()
+      currentPosition += width
+      draw()
+      freeze()
+    }
   
