eebbedde9400f828a6896d91db908a5125e7d2f7
Monsterlaser <73480524+Monsterlaser@users.noreply.github.com>
2021-10-08 16:46:39 -0400

    Update script.js

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,44 +1,48 @@
-async function windowActions({
-    const endpoint = '';
-    const cities = [];
-    const request = await fetch(endpoint)
-        .then(blob => blob.json())
-        .then(data => cities.push(...data))
-        const arrayName = await request.json()
+async function windowActions(){
+    const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+    const request = await fetch(endpoint);
+    const arrayName = await request.json()
+
     function findMatches(wordToMatch, cities) {
         return cities.filter(place => {
 
             const regex = new RegExp(wordToMatch, 'gi');
-            return place.city.match(regex) || place.state.match(regex)
+            return place.city.match(regex) || place.category.match(regex)
 
         });
     }
 
-    function numberWithCommas(x) {
-        return x.toString().repace(/\B(?=(d{3})+(?!\d))/g,',')
-    }
-
     function displayMatches(event){
-        const matchArray = findMatches(this.value, cities);
+        const matchArray = findMatches(event.target.value, arrayName)
         const html = matchArray.map(place => {
             const regex = new RegExp(event.target.value, 'gi');
-            const cityName = place.city.replace(regex, `<span class='h1>${event.target.value}</span>`);
-            const stateName = place.state.replace(regex, `<span class='h1>${event.target.value}</span>`);
             return `
-                <li>
-                    <span class='name'>${cityName}, ${stateName}</span>
-                    <span class='population'>${numberWithCommas(place.population)}</span>
-                </li>
-            `;
+                <ul>
+                    <li><div class="name">${place.name}</div></li>
+                    <div class="category">${place.category}</div>
+                    <div class="address">${place.address_line_1}</div>
+                    <div class="city">${place.city}</div>
+                    <div class="zip">${place.zip}</div>
+                </ul>
+                <br>
+                `
+
         }).join('');
-        suggestions.innerHTML = html;
+        if(event.target.value === '') {
+            suggestions.innerHTML = '';
+            
+        } else {
+            suggestions.innerHTML = html;
+        
     }
 
     const searchInput = document.querySelector('.search');
-    const searchInput = document.querySelector('.suggestions');
+    const suggestions = document.querySelector('.suggestions');
 
     searchInput.addEventListener('change', displayMatches);
-    searchInput.addEventListener('keyup', (evt) => { displayMatches(evt) });
+    searchInput.addEventListener('keyup', (evt) => { displayMatches(evt); });
 
-    window.onload = windowActions;
-})
+}
+
+
+window.onload = windowActions;
