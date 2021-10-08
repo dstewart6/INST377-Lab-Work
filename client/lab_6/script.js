e1e0c9aed90a832c3da94e52ba7af3b15a785052
Monsterlaser <73480524+Monsterlaser@users.noreply.github.com>
2021-10-08 17:56:43 -0400

    Update script.js

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,22 +1,22 @@
-async function windowActions(){
-    const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-    const request = await fetch(endpoint);
-    const arrayName = await request.json()
+async function windowActions() {
+  const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+  const request = await fetch(endpoint);
+  const arrayName = await request.json();
+  const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
 
-    function findMatches(wordToMatch, cities) {
-        return cities.filter(place => {
+  function findMatches(wordToMatch, cities) {
+    return cities.filter((place) => {
+      const regex = new RegExp(wordToMatch, 'gi');
+      return place.city.match(regex) || place.category.match(regex);
+    });
+  }
 
-            const regex = new RegExp(wordToMatch, 'gi');
-            return place.city.match(regex) || place.category.match(regex)
-
-        });
-    }
-
-    function displayMatches(event){
-        const matchArray = findMatches(event.target.value, arrayName)
-        const html = matchArray.map(place => {
-            const regex = new RegExp(event.target.value, 'gi');
-            return `
+  function displayMatches(event) {
+    const matchArray = findMatches(event.target.value, arrayName);
+    const html = matchArray.map((place) => {
+      const regex = new RegExp(event.target.value, 'gi');
+      return `
                 <ul>
                     <li><div class="name">${place.name}</div></li>
                     <div class="category">${place.category}</div>
@@ -25,24 +25,17 @@ async function windowActions(){
                     <div class="zip">${place.zip}</div>
                 </ul>
                 <br>
-                `
-
-        }).join('');
-        if(event.target.value === '') {
-            suggestions.innerHTML = '';
-            
-        } else {
-            suggestions.innerHTML = html;
-        
+                `;
+    }).join('');
+    if (event.target.value === '') {
+      suggestions.innerHTML = '';
+    } else {
+      suggestions.innerHTML = html;
     }
 
-    const searchInput = document.querySelector('.search');
-    const suggestions = document.querySelector('.suggestions');
-
-    searchInput.addEventListener('change', displayMatches);
-    searchInput.addEventListener('keyup', (evt) => { displayMatches(evt); });
-
+   
+  }
+  searchInput.addEventListener('keyup', (evt) => { displayMatches(evt); });
 }
-
-
 window.onload = windowActions;
+console.log('TEST')
