0d3e0667156191eed4a524b888951460cd55189d
Monsterlaser <73480524+Monsterlaser@users.noreply.github.com>
2021-10-16 23:51:23 -0400

    Update script.js

---
+++ client/lab_7/script.js
@@ -0,0 +1,38 @@
+async function windowActions() {
+  const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+  const request = await fetch(endpoint);
+  const arrayName = await request.json();
+  const searchInput = document.querySelector('.search');
+  const suggestions = document.querySelector('.suggestions');
+
+  function findMatches(wordToMatch, cities) {
+    return cities.filter((place) => {
+      const regex = new RegExp(wordToMatch, 'gi');
+      return place.city.match(regex) || place.category.match(regex);
+    });
+  }
+
+  function displayMatches(event) {
+    const matchArray = findMatches(event.target.value, arrayName);
+    const html = matchArray.map((place) => {
+      const regex = new RegExp(event.target.value, 'gi');
+      return `
+                <ul>
+                    <li><div class="name">${place.name}</div></li>
+                    <div class="category">${place.category}</div>
+                    <div class="address">${place.address_line_1}</div>
+                    <div class="city">${place.city}</div>
+                    <div class="zip">${place.zip}</div>
+                </ul>
+                <br>
+                `;
+    }).join('');
+    if (event.target.value === '') {
+      suggestions.innerHTML = '';
+    } else {
+      suggestions.innerHTML = html;
+    }
+  }
+  searchInput.addEventListener('keyup', (evt) => { displayMatches(evt); });
+}
+window.onload = windowActions;
